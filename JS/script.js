c5af786a0c5c3196bce36f38e301c61f2e0c367f
unknown <xamlol2982002@gmail.com>
2021-12-08 10:49:28 +0700

    update script file

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,12 @@
 const wrapper = document.querySelector(".wrapper"),
 musicImg = wrapper.querySelector(".img-area img"),
 musicName = wrapper.querySelector(".song-details .name"),
-musicArtist = wrapper.querySelector(".song-details .artist");
+musicArtist = wrapper.querySelector(".song-details .artist"),
+mainAudio = wrapper.querySelector("#main-audio"),
+playPauseBtn = wrapper.querySelector(".play-pause"),  
+preBtn = wrapper.querySelector("#prev"),
+nextBtn = wrapper.querySelector("#next"),
+progressBar =wrapper.querySelector(".progress-bar");
 
 let musicIndex = 1;
 
@@ -12,4 +17,57 @@ function loadMusic(indexNumb){
     musicName.innerText= allMusic[indexNumb-1].name;
     musicArtist.innerText = allMusic[indexNumb-1].artist;
     musicImg.src = `/Music-Player/Images/${allMusic[indexNumb-1].img}.PNG`;
+    mainAudio.src =  `/Music-Player/Songs/${allMusic[indexNumb-1].src}.mp3`;
 }
+
+function playMusic(){
+    wrapper.classList.add("paused");
+    playPauseBtn.querySelector("i").innerText = "pause";
+    mainAudio.play();
+}
+function pauseMusic(){
+    wrapper.classList.remove("paused");
+    playPauseBtn.querySelector("i").innerText = "play_arrow";
+    mainAudio.pause();
+}
+function nextMusic(){
+    musicIndex++;
+    musicIndex > allMusic.length ? musicIndex = 1 : musicIndex = musicIndex; 
+    loadMusic(musicIndex);
+    playMusic();
+}
+function prevMusic(){
+    musicIndex--;
+    musicIndex < 1 ? musicIndex = allMusic.length : musicIndex = musicIndex; 
+    loadMusic(musicIndex);
+    playMusic();
+}
+
+playPauseBtn.addEventListener("click", ()=>{
+    const isMusicPlay = wrapper.classList.contains("paused");
+    isMusicPlay ? pauseMusic() : playMusic();
+})
+
+nextBtn.addEventListener("click", ()=>{
+    nextMusic();
+});
+
+preBtn.addEventListener("click", ()=>{
+    prevMusic();
+});
+
+mainAudio.addEventListener("timeupdate", (e)=>{
+    const currentTime = e.target.currentTime;
+    const duration = e.target.duration;
+    let progressWidth = (currentTime/duration) * 100;
+    progressBar.style.width = `${progressWidth}%`;
+
+    mainAudio.addEventListener("loadeddata", ()=>{
+        let musicCurrentTime = wrapper.querySelector(".current"),
+        musicDuration = wrapper.querySelector(".duration");
+        let totalMin = Math.floor(audioDuration / 60);
+        let totalSec = Math.floor(audioDuration % 60);
+        let audioDuration = mainAudio.duration;
+        musicDuration.innerText = audioDuration;
+    });
+});
